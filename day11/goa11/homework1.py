60ddf515192927bd3fccbd7200ec1aeda2335e91
giorgiskhirtladze <gskhirtladze65@gmail.com>
2023-11-30 22:10:33 +0400

    first commit

--- day11/goa11/homework1.py
+++ day11/goa11/homework1.py
@@ -1,8 +1,8 @@
 def checking_the_triangle(a,b,c) :
     if a + b < c :
         print('triangle will not exist')
-else:
-  print('it will exist')
+    else:
+        print('it will exist')
 
 side_1 = int(input('enter side one: '))
 side_2 = int(input('enter side two: '))
--- day7/homework/goa7.py
+++ day7/homework/goa7.py
@@ -2,6 +2,6 @@ tickets = 25
 children = 3
 adult = 10
 
-print(adult * tickets + children * o )
+print(adult * tickets + children * 0 )
 
 
